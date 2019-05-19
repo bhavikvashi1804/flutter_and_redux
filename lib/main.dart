98a5f8e5dd6f478b14bc79d6295255b6c4fdbffb
Bhavik Vashi <bhavikvashi1804@gmail.com>
2019-05-19 11:12:55 +0530

    initial commit 1

--- lib/main.dart
+++ lib/main.dart
@@ -10,55 +10,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
             primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage("Flutter Demo Home Page"),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  String title;
 
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-       _counter++;
-    });
-  }
+  MyHomePage(this.title);
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-      appBar: AppBar(
-          title: Text(widget.title),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: Text("Fluuer+Redux"),centerTitle: true,),
       body: Center(
-         child: Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Text("Count"),
+            Text("0"),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: ()=> print(" iam pressed"),
         child: Icon(Icons.add),
+
       ),
+
     );
   }
 }
